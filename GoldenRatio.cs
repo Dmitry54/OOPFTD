af709b0d8c7d8f844af851ce2c3798540cf1383b
Dmitry54 <91108901+Dmitry54@users.noreply.github.com>
2021-10-06 12:01:25 +0700

    Add files via upload

---
+++ GoldenRatio.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+class GoldenRatio
+{
+    static double Func(double x)
+    {
+        double y = 0;
+        y = Math.Pow((x + 1), 3) + 5 * Math.Pow(x, 2);
+        return y;
+    }
+    public double FindMin(double a, double b, double e)
+    {
+        double xmin = 0;
+        double x1 = 0;
+        double x2 = 0;
+        double f1 = 0;
+        double f2 = 0;
+        double ratio = (-1 + Math.Sqrt(5)) / 2;
+        while (e <= Math.Abs(b - a))
+        {
+            x1 = a + (1 - ratio) * (b - a);
+            x2 = a + ratio * (b - a);
+            f1 = Func(x1);
+            f2 = Func(x2);
+            if (f1 >= f2)
+            {
+                a = x1;
+                xmin = x2;
+            }
+            else
+            {
+                b = x2;
+                xmin = x1;
+            }
+
+        }
+        return xmin;
+    }
+    public double FindMax(double a, double b, double e)
+    {
+        double xmax = 0;
+        double x1 = 0;
+        double x2 = 0;
+        double f1 = 0;
+        double f2 = 0;
+        double ratio = (-1 + Math.Sqrt(5)) / 2;
+        while (e <= Math.Abs(b - a))
+        {
+            x1 = a + (1 - ratio) * (b - a);
+            x2 = a + ratio * (b - a);
+            f1 = Func(x1);
+            f2 = Func(x2);
+            if (f1 <= f2)
+            {
+                a = x1;
+                xmax = x2;
+            }
+            else
+            {
+                b = x2;
+                xmax = x1;
+            }
+
+        }
+        return xmax;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalcBox
+{
+    
+    class Calculation
+    {
+        
+
+        static void Main(string[] args)
+        {
+            GoldenRatio gr = new GoldenRatio();
+            double e = 0.001;
+            double a = -2;
+            double b = 2;
+            double ratio = (-1 + Math.Sqrt(5)) / 2;
+            double xmin = 0;
+            double xmax = 0;
+            xmin = gr.FindMin(a, b, e);
+            xmax = gr.FindMax(a, b, e);
+            Console.WriteLine(xmin);
+            Console.WriteLine(xmax);
+            Console.ReadLine();
+        }
+    }
+
+}
